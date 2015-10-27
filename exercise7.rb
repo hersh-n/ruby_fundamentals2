c52cc2034adbcdbfac1f086df5ace6da49c34914
Hershel Nashman <hershel.nashman@gmail.com>
2015-10-27 17:48:07 -0400

    new method that prints out has

---
+++ exercise7.rb
@@ -0,0 +1,12 @@
+def display(cohort_list)
+	cohort_list.each do |cohort, students|
+		puts "#{cohort}: #{students} students"
+	end
+end
+
+students = {
+  :cohort1 => 34,
+  :cohort2 => 42,
+  :cohort3 => 22
+}
+display(students)
