16601371484d90e86665878f607589967f89a0fa
Hershel Nashman <hershel.nashman@gmail.com>
2015-10-27 17:08:24 -0400

    added a list counter

--- exercise6.rb
+++ exercise6.rb
@@ -2,11 +2,13 @@
 
 def print(shopping_list)
 	shopping_list.each {|item| puts "* " + item}
+	puts shopping_list.count
 end
 shopping_list = ["banana", "peanut", "potato"]
 shopping_list.push("rice")
 
 print(shopping_list)
+
 
 
 
