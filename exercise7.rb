b2634d76f31a8419461fbdbf4f2d1ccaa70f7cba
Hershel Nashman <hershel.nashman@gmail.com>
2015-10-28 15:36:56 -0400

    total number of students gets printed

--- exercise7.rb
+++ exercise7.rb
@@ -3,8 +3,21 @@ def display(cohort_list)
 		value2 = value.to_f + (value.to_f * 0.05)
 		puts "#{key}: #{value2} students"
 	end
-
 end
+
+def sum (hash)
+	sum = 0
+	hash.each do | key, value |
+		sum += value
+	end
+	return sum
+end
+
+
+
+
+
+
 
 students = {
   :cohort1 => 34,
@@ -18,3 +31,8 @@ students.delete(:cohort2)
 display(students)
 
 puts students.keys
+
+puts "The total number if students in all the cohorts is #{sum(students)}"
+
+#puts students.values.inject(:+)
+
